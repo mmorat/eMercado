9d9cfe89c7128486d99f89d0d7e0287977b237f6
Alidoandres <141370844+Alidoandres@users.noreply.github.com>
2023-08-17 20:11:17 -0300

    ss
    aa

--- js/login.js
+++ js/login.js
@@ -1,17 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(){ 
 
+document.addEventListener("DOMContentLoaded", function(){
     const loginForm = document.getElementById("loginForm");
+  
     loginForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-      const username = document.getElementById("username").value;
-      if (username.trim() !== "") {
-        // Almacenar el nombre en sessionStorage para usarlo en home.js
-        sessionStorage.setItem("username", username);
-        // Redirigir a la página principal
-        window.location.href = "index.html";
-      } else {
-        alert("El usuario no está logueado, por favor rellene los campos.");
-      }
-    });
+        event.preventDefault(); 
+  
+        const usernameInput = document.getElementById("username");
+        const passwordInput = document.getElementById("password");
+  
+        const username = usernameInput.value.trim();
+        const password = passwordInput.value.trim();
+  
+        if (username === "" || password === "") {
+            alert("Por favor rellene los campos");
+        } else {
+            sessionStorage.setItem("username", username);
+            window.location.href = "index.html";
+        }
+  
 
-  });
+})});
