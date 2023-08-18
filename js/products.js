41699278f3eb8d73f4807fd93f5dc9e661aa80d8
mmorat <137867297+mmorat@users.noreply.github.com>
2023-08-17 21:40:35 -0300

    más cambios
    products.js reestructuración del código para mayor legibilidad y flexibilidad
    products.html algunas partes comentadas ya que no son de utilidad en este momento pero pueden serlo en el futuro
    styles.css modificado

--- js/products.js
+++ js/products.js
@@ -1,21 +1,30 @@
-let autos = "https://japceibal.github.io/emercado-api/cats_products/101.json"
-fetch(autos)
-.then(response => response.json())
-.then(data => {
-    for(const product of data.products) {
-        const item = document.createElement("div.producto");
-        item.appendChild(document.createElement("img")).src = product.image
-        item.appendChild(document.createElement("h4")).textContent =
-        product.name + " - " + product.currency + "$ " + product.cost;
-        item.append(product.description);
-          myList.appendChild(item)
-    }
-    
-})
-.catch(error => console.log(error))
-
-
-
-
-
 const myList = document.getElementById("prodAutos");
+const autosUrl = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+fetch(autosUrl)
+  .then(response => response.json())
+  .then(data => {
+    data.products.forEach(product => {
+      const item = document.createElement("div");
+      item.classList.add("producto");
+
+      const img = document.createElement("img");
+      img.src = product.image;
+      item.appendChild(img);
+      
+      const soldCountDiv = document.createElement("div");
+      soldCountDiv.classList.add("sold-count")
+      soldCountDiv.textContent = `${product.soldCount} vendidos`;
+      item.appendChild(soldCountDiv);
+
+      const h4 = document.createElement("h4");
+      h4.textContent = `${product.name} - ${product.currency}$ ${product.cost}`;
+      item.appendChild(h4);
+
+      item.appendChild(document.createTextNode(product.description));
+      
+
+      myList.appendChild(item);
+    });
+  })
+  .catch(error => console.log(error));
