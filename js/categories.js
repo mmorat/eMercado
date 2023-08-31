080d290977fcb972060cc2ba6d366e4e1e889fae
rzolsc <renzo.lescano@hotmail.com>
2023-08-31 20:13:01 -0300

    display user & recordar sesion
    agregado display user en index, categories y sell
    chechbox recordar sesión funcional

--- js/categories.js
+++ js/categories.js
@@ -140,4 +140,22 @@ document.addEventListener("DOMContentLoaded", function(e){
 
         showCategoriesList();
     });
+
+    // display username
+    
+    const username = sessionStorage.getItem("username") || localStorage.getItem("username");
+      
+    if (!username){
+        setTimeout(function() {
+            window.location.href = "login.html";
+        }, 3000);}
+
+    // busca el user-display y crea const para lo sig.
+    const userDisplayElement = document.getElementById('user-display');
+
+    // si existe un nombre de usuario, se muestra
+    if (username) {
+        userDisplayElement.textContent = username;
+    }
+    //
 });
