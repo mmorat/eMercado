1f5aca87e631aa3482b882b706e383d6b05ac795
mmorat <137867297+mmorat@users.noreply.github.com>
2023-08-31 19:36:25 -0300

    primer commit
    modifiicaciones al codigo de la entrega 1, que solo hacía fetch a la categoría autos, para generalizarlo.
    +  modificaciones para cumplir con el punto 2 de la entrega 2

--- js/products.js
+++ js/products.js
@@ -1,10 +1,15 @@
-const myList = document.getElementById("prodAutos");
-const autosUrl = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+document.addEventListener("DOMContentLoaded", function () {
+  const myList = document.getElementById("showProd");
+  const baseURL = "https://japceibal.github.io/emercado-api/cats_products/"; //url base
+  const catID = localStorage.getItem("catID"); //categoría de productos seleccionada guardada en localStorage
+  const puntoJSON = ".json"; //finalización del url
+  let URL = baseURL+catID+puntoJSON; //url final
 
-fetch(autosUrl)
+  
+fetch(URL)
   .then(response => response.json())
   .then(data => {
-    data.products.forEach(product => {
+      data.products.forEach(product => {
       const item = document.createElement("div");
       item.classList.add("producto");
 
@@ -23,8 +28,12 @@ fetch(autosUrl)
 
       item.appendChild(document.createTextNode(product.description));
       
-
       myList.appendChild(item);
     });
+
+    let categoria= data.catName;
+    document.getElementById("tituloProd").textContent+= ' "' + categoria + '"' //modificación del título de la página donde anuncia la categoría seleccionada
   })
+  
   .catch(error => console.log(error));
+});
