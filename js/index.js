63d02c34b1ba616c56356faca84045f1e1f6f37f
mmorat <137867297+mmorat@users.noreply.github.com>
2023-09-03 03:21:31 -0300

    Revert "barra buscadora + userdisplay + redireccionar a login"
    This reverts commit 60ceff34cf4bd5b67245b520f801f7f05a08f936.

--- js/index.js
+++ js/index.js
@@ -1,24 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-// display username
- 
-const username = sessionStorage.getItem("username") || localStorage.getItem("username");
-if (!username) {
-  alert("You must log in");
-  setTimeout(function () {
-    window.location.href = "login.html";
-  }, 2300);
-}
 
-  // busca el user-display y crea const para lo sig.
-  const userDisplayElement = document.getElementById('user-display');
-
-  // si existe un nombre de usuario, se muestra
-  if (username) {
-      userDisplayElement.textContent = username;
-  }
-
-  //
-  
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
@@ -31,5 +12,24 @@ if (!username) {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+
+        // display username
+    
+        const username = sessionStorage.getItem("username") || localStorage.getItem("username");
+      
+   if (!username){
+        alert ("usted debe hacer login")
+        setTimeout(function() {
+            window.location.href = "login.html";
+          }, 2300);}
+
+    // busca el user-display y crea const para lo sig.
+    const userDisplayElement = document.getElementById('user-display');
+
+    // si existe un nombre de usuario, se muestra
+    if (username) {
+        userDisplayElement.textContent = username;
+    }
+    //
 })
 
