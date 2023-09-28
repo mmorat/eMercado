1da0ab820d5034672c0762ce04af9c3412b2aa17
Viviana Teng <viviiteng.-@hotmail.com>
2023-09-28 18:44:34 -0300

    mini-changes

--- js/cart.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const username = sessionStorage.getItem("username") || localStorage.getItem("username");
-if (!username){
-    alert ("usted debe hacer login")
-    setTimeout(function() {
-        window.location.href = "login.html";
-      }, 2300);}
-})
--- js/products.js
+++ js/products.js
@@ -1,23 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-// display username
- 
-const username = sessionStorage.getItem("username") || localStorage.getItem("username");
-if (!username) {
-  alert("You must log in");
-  setTimeout(function () {
-    window.location.href = "login.html";
-  }, 2300);
-}
-
-  // busca el user-display y crea const para lo sig.
-  const userDisplayElement = document.getElementById('user-display');
-
-  // si existe un nombre de usuario, se muestra
-  if (username) {
-      userDisplayElement.textContent = username;
-  }
-
   //
   const lista = document.getElementById("showProd");
   const catID = localStorage.getItem("catID");
